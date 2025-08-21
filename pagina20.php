a65806664c6d9553642633c437fd534bd48a2912
GoibaGoiba <enzzocossolino@gmail.com>
2025-08-21 15:50:11 -0300

    Atividade sobre While

---
+++ pagina20.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3> Este é um While</h3>
+    <?php
+    $contador=1;
+        while ($contador<=5){
+            echo "Número: $contador<br>";
+            $contador++
+        }
+    $cont=1;
+    while ($cont<=5){
+        $cont++
+        echo "Número: $cont<br>";
+    }
+    ?>
+</body>
+</html>

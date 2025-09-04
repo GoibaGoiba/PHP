4ed341f79cff4b3f840b501af78054ad864b996f
GoibaGoiba <enzzocossolino@gmail.com>
2025-09-04 12:06:03 -0300

    Termindado a atividade

---
+++ Projeto1/Calculo_Nota.php
@@ -0,0 +1,14 @@
+<?php
+function nota($n1,$Disciplina){
+    if($n1>=7){
+        echo "Você foi aprovado na disciplina de ".$Disciplina.".";
+    } if($n1>=5 && $n1<7){
+        echo "Você está de recuperação em ".$Disciplina.".";
+    } if($n1<5){
+        echo "Você foi reprovado em ".$Disciplina.".";
+    }
+    }
+$n1=$_POST["Nota"];
+$Disciplina=$_POST["Disciplina"];
+nota($n1,$Disciplina);
+?>
---
+++ Projeto1/Nota.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verificador de nota</title>
+</head>
+<body>
+    <form action="pagina32(Calc Nota).php" method="post">
+        <Label><b>Nome da disciplina</b></Label><br>
+        <p><input type="text" name="Disciplina" id="Disciplina" required></p>
+        <label><b>Nota: </b></label>
+        <p><input type="number" name="Nota" id="Nota" required></P>
+        <input type="submit" value="Verificar">
+    </form>
+</body>
+</html>
